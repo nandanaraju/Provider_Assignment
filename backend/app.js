17908b27d3e0674257a343b67344f321c61cbc91
nandhuraju <nandhuraju6@gmail.com>
2025-03-21 08:38:34 +0530

    swagger.json

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./swagger');
-const providerRoutes = require('./src/routes/providerRoutes');
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const fs = require("fs");
+const path = require("path");
+const providerRoutes = require("./src/routes/providerRoutes");
 const sequelize = require("./src/config/database");
-const db = require('./models');
 require("dotenv").config();
+
 const app = express();
 app.use(express.json());
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use('/api/providers', providerRoutes);
+// Load Swagger JSON
+const swaggerPath = path.join(__dirname, "swagger", "swagger.json");
+const swaggerDocs = JSON.parse(fs.readFileSync(swaggerPath, "utf-8"));
 
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api/providers", providerRoutes);
 
-
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
 
 // Sync models
-sequelize.authenticate()
-  .then(() => console.log('Database connected'))
-  .catch(err => console.error('Error connecting to database:', err));
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Error connecting to database:", err));
